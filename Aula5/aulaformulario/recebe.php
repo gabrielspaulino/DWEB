887473bddddbe820d832eb1bd9b8d086da6979bc
Gabriel Paulino <gabrielpaulino06@hotmail.com>
2021-09-13 21:11:32 -0300

    1semestre

---
+++ Aula5/aulaformulario/recebe.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf8">
+<title>Documento sem titulo</title>
+</head>
+
+<body>
+    <?php
+echo $_POST ['nome'];
+
+?>
+
+</body>
+</html>
---
+++ ex 27 ago 20/recebe-formulario.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sem titulo</title>
+</head>
+
+<body>
+    <?php
+echo $_POST['nome'];
+echo $_POST['email'];
+echo $_POST['tel'];
+echo $_POST['site'];
+echo $_POST['nascimento'];
+echo $_POST['filhos'];
+echo $_POST['temp'];
+echo $_POST['hora'];
+echo $_POST['senha'];
+?>
+
+</body>
+</html>
